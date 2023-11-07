b4c6efdca9278265b81bc646f854f88d949b28f7
StephenAnthony8 <steveoti.tony@gmail.com>
2023-11-07 18:55:20 +0300

    .isoformat() changes & pycodestyle

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from models.base_model import BaseModel
 
 r1 = BaseModel()
 
-print(f'id = {r1.id}\ncreated_at = {r1.created_at}\nupdated_at = {r1.updated_at}')
+r1.name = "Omni man"
 print(r1.__dict__)
+print("---------------\n")
 print(r1)
+print("---------------\n")
 print(dir(BaseModel()))
+print("---------------\n")
 print(r1.to_dict())
 
--- models/base_model.py
+++ models/base_model.py
@@ -26,27 +26,31 @@ class BaseModel():
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, key,
+                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 elif key != '__class__':
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            iso_time = datetime.isoformat((datetime.now()))
+            iso_time = datetime.now()
             self.created_at = self.updated_at = iso_time
-            # updated_at should be modified anytime we make a change in the object
+            # updated_at - modified anytime we make a change in the object
 
     def __str__(self):
         """
             Prints ,class name. id & dict in human readable format
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__,
+                self.id, self.__dict__
+                )
 
     def save(self):
         """
             Public method
             - Updates the attr 'updated_at' with the current datetime
         """
-        self.updated_at = datetime.isoformat(datetime.now())
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
@@ -56,20 +60,6 @@ class BaseModel():
         """
         result_dict = dict(self.__dict__)
         result_dict['__class__'] = self.__class__.__name__
-        result_dict['created_at'] = self.created_at.isoformat()
-        result_dict['updated_at'] = self.updated_at.isoformat()
+        result_dict['created_at'] = datetime.isoformat(self.created_at)
+        result_dict['updated_at'] = datetime.isoformat(self.updated_at)
         return (result_dict)
-        #new_dict = self.__dict__
-        #new_dict['__class__'] = Rectangle.class_name()
-        #return (new_dict)
-
-    #@classmethod
-    #def class_name(cls):
-        """
-            class method
-            - Returns the class name
-            - if you can find a better way to go about this
-                by all means please do, and then share the procedure
-                cause hapa nafloat mbaya
-    """
-        #return (cls.__name__)
