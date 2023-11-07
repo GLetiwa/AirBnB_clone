bb6c68abb27934ad3af2de2b55e16d9c3caed225
Grace Letiwa <133238809+GLetiwa@users.noreply.github.com>
2023-11-07 13:32:49 +0300

    Update base_model.py
    My thoughts on the __dict__ method

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel():
         - Defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
             - BaseModel object instantiation
             - Creates an instance of type 'BaseModel' with params
@@ -23,13 +23,19 @@ class BaseModel():
                 - created_at - time instance was created
                 - updated_at - time when a change occurs in the instance
         """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            iso_time = datetime.isoformat((datetime.now()))
+            self.created_at = self.updated_at = iso_time
+            # updated_at should be modified anytime we make a change in the object
 
-        self.id = str(uuid.uuid4())
-        iso_time = datetime.isoformat((datetime.now()))
-        self.created_at = self.updated_at = iso_time
-        # updated_at should be modified anytime we make a change in the object
-
-    def __str__(self, name=None):
+    def __str__(self):
         """
             Prints ,class name. id & dict in human readable format
         """
@@ -48,13 +54,17 @@ class BaseModel():
             - returns a dictionary containing all keys & values of __dict__
             - also includes a new '__class__' key with class name
         """
+        result_dict = dict(self.__dict__)
+        result_dict['__class__'] = self.__class__.__name__
+        result_dict['created_at'] = self.created_at.isoformat()
+        result_dict['updated_at'] = self.updated_at.isoformat()
 
-        new_dict = self.__dict__
-        new_dict['__class__'] = Rectangle.class_name()
-        return (new_dict)
+        #new_dict = self.__dict__
+        #new_dict['__class__'] = Rectangle.class_name()
+        #return (new_dict)
 
-    @classmethod
-    def class_name(cls):
+    #@classmethod
+    #def class_name(cls):
         """
             class method
             - Returns the class name
@@ -62,4 +72,4 @@ class BaseModel():
                 by all means please do, and then share the procedure
                 cause hapa nafloat mbaya
     """
-        return (cls.__name__)
+        #return (cls.__name__)
