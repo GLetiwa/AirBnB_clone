2c4a8a921624fb3b966f2d73cbb2627103f27021
StephenAnthony8 <steveoti.tony@gmail.com>
2023-11-08 04:44:33 +0300

    final changes

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
-r1 = BaseModel()
-
+"""x = "BaseModel"
+r1 = eval("{}()".format(x))
+r2 = BaseModel()
 r1.name = "Omni man"
 print(r1.__dict__)
 print("---------------\n")
 print(r1)
-print("---------------\n")
-print(dir(BaseModel()))
-print("---------------\n")
+#print("---------------\n")
 print(r1.to_dict())
-
+print("---------------\n")"""
+f1 = FileStorage()
+"""print(f1.all())
+f1.new(r1)
+f1.new(r2)
+print(f1.all())"""
+f1.reload()
+print("Reload method:")
+for key in f1.all():
+    print(f1.all()[key].__dict__)
+    print("-------------------")
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+    init module
+    - Converts directory and its content into a package
+    - Contains an instance of  'FileStorage'
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
