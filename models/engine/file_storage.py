d6d787bfdc5700aa635860ff8bba548b6f907fff
StephenAnthony8 <steveoti.tony@gmail.com>
2023-11-08 04:44:10 +0300

    final changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 import json
+
 
 class FileStorage:
     """serializes instances to a JSON file and deserializes JSON file to instances"""
@@ -9,17 +9,17 @@ class FileStorage:
 
     def all(self):
         """returns dictionary __object"""
-        return self.__object
+        return self.__objects
 
     def new(self, obj):
-        """sets and object in the dictionary with key <obj class name>.id"""
+        """sets an object in the dictionary with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__object[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        #craete a dictionary to hold serialized objects
-        obj_dict = {key: obj.to_dict for key, obj in self.__object.items()}
+        #create a dictionary to hold serialized objects
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         #open json file for writing
         with open (self.__file_path, "w", encoding='utf-8') as file:
             """write ob_dict to the file"""
@@ -27,11 +27,20 @@ class FileStorage:
 
     def reload(self):
         """ deserializes json file to object"""
+        from models.base_model import BaseModel
         try:
             with open(self.__file_path, "r") as file:
                 d_instances = json.load(file)
 
             for key in d_instances:
+                obj_name = d_instances[key]['__class__']
+                obj_attr = d_instances[key]
+
+                self.__objects[key] = eval(
+                        "{}(**{})".format(obj_name,obj_attr)
+                        )
+                # creation of the objects
+                # Not sure if there's a better way to serialise them
 
 
         except FileNotFoundError:
