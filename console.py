e35154faf74b1df7035892c04e5a10dfd1f4b850
StephenAnthony8 <steveoti.tony@gmail.com>
2023-11-10 10:28:39 +0300

    almost complete console

--- console.py
+++ console.py
@@ -42,7 +42,68 @@ class HBNBCommand(cmd.Cmd):
         sys.exit()
 
     def default(self, arg):
-        """ default - Interpret instance based commands """
+
+        string = HBNBCommand.args_check(arg)
+
+        if (string is None):
+            return
+        
+        else:
+            obj, command, param = string
+        
+        if (command != 'count'):
+            func = eval("self.do_{}".format(command))
+        
+        if (command == "all"):
+            func(obj)
+
+        elif (command in ["show", "destroy"]):
+            param = "{} {}".format(obj, param.strip('"'))
+            func(param)
+        
+        elif (command == "update"):
+            id_dict = HBNBCommand.convert_dict(param)
+
+            if (id_dict):
+                id_no, d_attr = id_dict
+
+                for key in d_attr:
+                    string = "{} {} {} {}".format(obj, id_no, key, d_attr[key])
+                    self.do_update(string)
+
+
+        else:
+            HBNBCommand.count(obj)
+
+    @classmethod
+    def args_check(cls, arg):
+        string = re.fullmatch(r"(.+)\.([a-z]{3,7})\((.*)\)", arg)
+        commands = ['all', 'count', 'show', 'destroy', 'update']
+
+        #syntax and command check
+        if (string is None or string.group(2) not in commands):
+            print("*** Unknown syntax: {}".format(arg))
+            return
+
+        #obj check
+        if (string.group(1) not in cls.all_classes):
+            print("** class doesn't exist **")
+            return
+        
+        #instance check
+        if (string.group(3) == "" and string.group(2) in commands[2:]):
+            print("** instance id missing **")
+            return
+        
+        #all & count check
+        if (string.group(3) != "" and string.group(2) in commands[:2]):
+            print("*** Unknown syntax: {}".format(arg))
+            return
+        
+        return (string.groups())
+
+    """def default(self, arg):
+        " default - Interpret instance based commands "
         commands = ['all', 'show', 'destroy', 'update']
 
         string = re.fullmatch(r'([A-Za-z]{4,9})\.([a-z]{3,7})\(("?.*"?)\)', arg)
@@ -62,17 +123,17 @@ class HBNBCommand(cmd.Cmd):
 
             elif (command == 'update'):
                 try:
-                    id_no, dict_param = HBNBCommand().convert_dict(param)
+                    id_no, attraram = HBNBCommand().convert_dict(param)
                 except (ValueError):
                     return
                 
-                for key in dict_param:
-                    string = '{} {} {} "{}"'.format(obj, id_no, key, dict_param[key])
+                for key in attraram:
+                    string = '{} {} {} "{}"'.format(obj, id_no, key, attraram[key])
                     self.do_update(string)
             else:
                 print("*** Unknown syntax: {}".format(arg))
         else:
-            print("*** Unknown syntax: {}".format(arg))
+            print("*** Unknown syntax: {}".format(arg))"""
 
     def do_create(self, arg):
         "create- Creates & saves an Object(to a JSON file) & prints it's ID"
@@ -169,24 +230,45 @@ class HBNBCommand(cmd.Cmd):
     @staticmethod
     def convert_dict(params):
         dict_args = dict()
+        d_keys = storage.all().keys()
 
-        if (re.fullmatch(r'"(.+)", "(.+)", (.+)', params)):
-            id_no, attr, value = eval(params)
-            dict_args[attr] = value
+        id_no = re.search(r'^"([^"]*)"', params)
+        attr = re.search(r'^"[^"]*", "([\w]+)"', params)
+        value = re.search(r'^"[^"]*", "[\w]+", (.*)', params)
+        str_dict = re.search(r'^"[^"]*", (\{.+})', params)
 
-        elif (re.fullmatch(r'"(.+)", (\{.*\})', params)):
-            id_no, dict_args = eval(params)
+        if (id_no is not None):
+            if (str_dict is not None):
 
+                return (id_no.group(1), str_dict.group(1))
+
+            elif (attr is not None):
+                if (value):
+                    dict_args = {attr.group(1) : value.group(1)}
+                    return (id_no.group(1),dict_args)
+                
+                else:
+                    print('** value missing **')
+                    return
+
+            else:
+                print('** attribute name missing **')
+                return
         else:
-            return (None)
+            print('** no instance found **')
 
-        return (id_no, dict_args)
+    @classmethod
+    def count(cls, arg):
+        "count - prints number of existing specified instance"
 
-    """@staticmethod
-    def update_err(params, type):
-        if (re.search()):
-            if (re.search()):
-                if ()"""
+        value = 0
+        if (arg == "") or (arg in cls.all_classes):
+            for key in storage.all():
+                if (arg in key):
+                    value += 1
+            print(value)
+        else:
+            print("** class doesn't exist **")
 
 
 if __name__ == "__main__":
