8bc89c797e4868bc24f25fdbf115562f5aaf62e6
GLetiwa <graceletiwa@gmail.com>
2023-11-12 09:37:54 -0800

    pycode fix with indentation error

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from models.base_model import BaseModel
 
 from models.user import User
+
+
 class TestUser(unittest.TestCase):
     def setUp(self):
         # Create an instance of the User for testing
@@ -31,19 +33,19 @@ class TestUser(unittest.TestCase):
             self.assertTrue(type(attr) is value)
 
     def test_str_representation(self):
-        # Test if the __str__ method produces the expected string representation
+        # Test if __str__ produces the expected string representation
         expected_str = "[User] ({}) {}".format(self.user.id, self.user.__dict__)
         self.assertEqual(str(self.user), expected_str)
 
     def test_to_dict_method(self):
-        # Test if the to_dict method returns a dictionary with expected keys/values
+        # Test if to_dict returns a dictionary with expected keys/values
         user_dict = self.user.to_dict()
         self.assertIsInstance(user_dict, dict)
         self.assertEqual(user_dict['__class__'], 'User')
         self.assertEqual(user_dict['id'], self.user.id)
 
     def test_to_dict_includes_class_name(self):
-        # Test if the to_dict method includes the __class__ key with the correct class name
+        # Test if to_dict includes the class key with the correct class name
         model_dict = self.user.to_dict()
         self.assertEqual(model_dict['__class__'], 'User')
 
@@ -65,29 +67,28 @@ class TestUser(unittest.TestCase):
 
     def test_updated_at_after_save(self):
         # Test if created_at remains the same after calling save
-        updated_time = self.user.updated_at 
+        updated_time = self.user.updated_at
         self.user.save()
         self.assertNotEqual(updated_time, self.user.updated_at)
 
     def test_object_creation_with_args(self):
-        #creates an object when *args is passed in that doesn't have it as args
+        # creates an object when args is passed in that doesn't have it as args
         l_arg = ['2023-11-11T06:02:57.369856', '2023-11-11T06:02:57.369856'
                  "Grace", "Letiwa", "letiwa@softwareengineer" "Nairobi"]
         obj = User(*l_arg)
 
         for key in self.attributes:
-            self.assertTrue(hasattr(obj,key))
-
+            self.assertTrue(hasattr(obj, key))
 
         for i in l_arg:
             self.assertNotEqual(i, obj.id)
             self.assertNotEqual(i, obj.updated_at)
-            self.assertNotEqual(i,obj.created_at)
-        
+            self.assertNotEqual(i, obj.created_at)
+
         del obj
-    
+
     def test_object_creation_with_kwargs_complete(self):
-        #creates an object when *kwargs is passed in
+        # creates an object when *kwargs is passed in
 
         d_kwargs = {
             "id": "bca8e814-2fbc-47f0-8c29-1baf7c98afce",
@@ -98,11 +99,11 @@ class TestUser(unittest.TestCase):
             'first_name': 'Grace',
             'last_name': 'Letiwa'
             }
-        
+
         obj = User(**d_kwargs)
 
         attributes = ['email', 'password', 'first_name', 'last_name']
-        #check if original attributes are included
+        # check if original attributes are included
         for i in attributes:
             self.assertTrue(hasattr(obj, i))
 
@@ -110,31 +111,32 @@ class TestUser(unittest.TestCase):
             self.assertTrue(hasattr(obj, key))
 
     def test_is_child_instance(self):
-        #tests if it is a child isntance of BaseModel
+        # tests if it is a child isntance of BaseModel
 
         self.assertTrue(isinstance(User(), BaseModel))
         self.assertFalse(type(User()) is BaseModel)
 
-    def test_update_attributes(self):#add to the rest
+    def test_update_attributes(self):
         # Test if updating attributes works as expected
         d_args = {
             'email': "test@example.com", 'password': "password123",
             'first_name': "Steve", 'last_name': "Tony"
             }
-        
+
         for key, value in d_args.items():
             setattr(self.user, key, value)
-        
+
         for key, value in d_args.items():
             attr = getattr(self.user, key)
             self.assertEqual(attr, value)
 
-    def test_default_values(self):#add to the rest
+    def test_default_values(self):
         # Test if the default values are set correctly
         for key in self.attributes:
             if key in ['email', 'password', 'first_name', 'last_name']:
                 value = getattr(self.user, key)
                 self.assertEqual(value, "")
+
 
 if __name__ == '__main__':
     unittest.main()
